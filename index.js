2647094e3cb7707de7db6134ced14866cb33c8cd
Asher <asherwoodrae@gmail.com>
2023-01-20 22:04:40 +0000

    made computer's logic work properly

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// Computer's answer logic
-answer = function randomNumber(answer) {
-    Math.floor(Math.random() * 100) +1;
+// // Computer's answer logic
+function getRandomItem(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    const item = arr[randomIndex];
+    return item;
 }
-if (answer <= 33) {
-    foeAnswer = "rock";
-} else if ((answer > 33) && (answer <=66)) {
-    foeAnswer = "paper";
-} else {
-    foeAnswer = "scissors";
-};
+const array = ['rock', 'paper', 'scissors',];
+const result = getRandomItem(array);
+console.log(result);
+
+// // User's answer logic
+
+// // let userAnswer = document.querySelector('.userAnswer')
+// userAnswer = prompt("What is your answer?")
+// // if (userAnswer !== "rock" || userAnswer !== "paper" || userAnswer !== "scissors") {
+// //     alert("You have to type a valid answer!")
+// // }
 
 
-// User's answer logic
-// let userAnswer = document.querySelector('.userAnswer')
-userAnswer = prompt("What is your answer?")
-if (userAnswer.toLowerCase() !== "rock" || userAnswer.toLowerCase() !== "paper" || userAnswer.toLowerCase() !== "scissors") {
-    alert("You have to type a valid answer!")
-}
-
-
-//Game logic
-if (foeAnswer == userAnswer) {
-    alert("It's a tie!")
-}
+// //Game logic
+// if (foeAnswer == userAnswer) {
+//     alert("It's a tie!")
+// }
