29718da899dfb26b8553271a55fe1608b90d8bd7
Asher <asherwoodrae@gmail.com>
2023-01-22 15:29:02 +0000

    switched the randomising logic to if statement for readability

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
-// // Computer's answer logic
-function getRandomItem(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    const item = arr[randomIndex];
-    return item;
-}
-const array = ['rock', 'paper', 'scissors',];
-let computerChoice = getRandomItem(array);
-console.log("The computer chose : " + computerChoice);
 
-
-// // User's answer logic
-let userChoice = prompt("What is your choice?")
-console.log("You chose : " + userChoice);
-if (userChoice == null || userChoice == undefined){
-    console.log("Only ROCK, PAPER, or SCISSORS may be entered"),
-    console.log("This round has been terminated (you dick)")
+// Function to randomly generate the computer's answer
+function computerPlay() {
+    randomGameValue = Math.floor(Math.random() * 3);
+    if (randomGameValue == "0") {
+        return "rock";
+    }
+    else if (randomGameValue == "1") {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
 }
 
-// game logic
-function playRound(userChoice, computerChoice) {
+// Function for a single round of the main game
+function playRound() {
+    // Asking for player's selection
+    let playerSelection = prompt("Rock, Paper, or Scissors?")
+          // Generate computer selection
+          const computerSelection = computerPlay();
+}
+
+// User's answer logic
+function playerPlay() {
+    let playerSelection = prompt('∘• rock | paper | scissors •∘').toLowerCase();
+    if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
+      console.log(`Your choice is ${playerSelection}.`)
+      return playerSelection;
+    }
+    console.log(`YOU CAN'T TYPE ${playerSelection}.VALID WORDS ARE  ROCK | PAPER | SCISSORS`);
+    console.log('%cTHIS ROUND OF THE GAME WAS DESTROYED (╯°益°)╯彡┻━┻', 'color: #ef8513');
+  }
+
+
+// single round game logic
+function outCome(playerPlay(), computerPlay()) {
     // if user picks rock
     if ((userChoice == "rock")&&(computerChoice == "paper")){
-        return ("You lose! Paper beats rock");
+        console.log("You lose! Paper beats rock");
     } else if ((userChoice == "rock")&&(computerChoice == "scissors")){
-        return ("You win! Rock beats scissors");
+        console.log("You win! Rock beats scissors");
     } else if ((userChoice == "rock")&&(computerChoice == "rock")){
-        return ("You tied! Rock is equal to rock"); 
+        console.log("You tied! Rock is equal to rock"); 
     } // if user picks paper
     else if ((userChoice == "paper")&&(computerChoice == "scissors")){
-        return ("You lose! Scissors beats paper");
+        console.log("You lose! Scissors beats paper");
     } else if ((userChoice == "paper")&&(computerChoice == "rock")){
-        return ("You win! Paper beats rock");
+        console.log("You win! Paper beats rock");
     } else if ((userChoice == "paper")&&(computerChoice == "paper")){
-        return ("You tied! Paper is equal to paper");
+        console.log("You tied! Paper is equal to paper");
     } // if the user picks scissors
     else if ((userChoice == "scissors")&&(computerChoice == "rock")){
-        return ("You lose! Rock beats scissors");
+        console.log("You lose! Rock beats scissors");
     } else if ((userChoice == "scissors")&&(computerChoice == "paper")){
-        return ("You win! Scissors beats paper");
+        console.log("You win! Scissors beats paper");
     } else if ((userChoice == "scissors")&&(computerChoice == "scissors")){
-        return ("You tied! Scissors are equal to scissors");} 
+        console.log("You tied! Scissors are equal to scissors");} 
     }
-console.log(playRound(userChoice, computerChoice))
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// multiple round game logic
+    for (let i = 1; i < 6; i++) {
+         outCome(playerPlay(), computerPlay());
+         console.log("You are on round : " + i);
+        }
